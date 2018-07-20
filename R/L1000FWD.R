d19d94dd3ecad2d2e88e5765e19bb9f33910a866
OganM <ogan.mancarcii@gmail.com>
2018-07-19 19:48:28 -0700

    fix up FWD

--- R/L1000FWD.R
+++ R/L1000FWD.R
@@ -1,9 +1,34 @@
-# currently broken
+#' @export
 L1000FWD = function(upGenes,downGenes){
     L1000FWD_URL <- 'http://amp.pharm.mssm.edu/L1000FWD/'
 
     payload = list(up_genes = upGenes,
                    down_genes = downGenes)
-    response <- httr::POST(paste0(L1000FWD_URL, 'sig_search/'), body=payload, encode='json')
+    response <- httr::POST(paste0(L1000FWD_URL, 'sig_search'), body=payload, encode='json')
+
+    response <- jsonlite::fromJSON(httr::content(response, 'text'))
+
+    response = httr::GET(paste0(L1000FWD_URL,'result/topn/',response$result_id))
+
+    response <- jsonlite::fromJSON(httr::content(response, 'text'))
+
+
+
+    response %<>% lapply(function(x){
+        chemInfo = x$sig_id %>% sapply(function(y){
+            response = httr::GET(paste0(L1000FWD_URL,'sig/',y))
+            response <- jsonlite::fromJSON(httr::content(response, 'text'))
+
+            return(c(pert_desc = response$pert_desc,
+                     pert_dose = response$pert_dose,
+                     cell_id = response$cell_id,
+                     pert_id = response$pert_id,
+                     pert_time = response$pert_time
+                     ))
+        })
+
+        cbind(x, t(chemInfo))
+    })
 
 }
+
